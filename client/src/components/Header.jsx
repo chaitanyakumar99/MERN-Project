511a2b555be472e6e2d781cbb7ec1560612aab99
Your Name <gchkumar999@gmail.com>
2025-03-16 12:28:05 +0530

    txt

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { assets } from '../../assets/assets'
+
+const Header = () => {
+  return (
+    <div className='flex flex-col items-center mt-20 px-4 text-center text-gray-800'>
+      <img src={assets.header_img} alt="" className='w-36 h-36 rounded-full mb-6' />
+      <h1>Hey Developer <img className='w-8 aspect-square' src={assets.hand_wave} alt="" /></h1>
+      <h2>Welcome to our app</h2>
+      <p>Let's start with a quick product tour and we will have you up and running in no time!</p>
+      <button>Get Started</button>
+    </div>
+  )
+}
+
+export default Header
