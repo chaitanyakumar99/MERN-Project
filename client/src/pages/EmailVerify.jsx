45d5bfb2bc30bf495c1cb9b5914116758919e37b
Your Name <gchkumar999@gmail.com>
2025-03-18 00:11:56 +0530

    txt

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -27,9 +27,9 @@ const EmailVerify = () => {
                     {Array(6).fill(0).map((_, index) => (
                         <input type="text" maxLength='1' key={index} required
                             className='w-12 h-12 bg-[#333A5C] text-white text-center text-xl rounded-md'
-                            ref={e => inputRefs.current[index] = e}
-                            onInput={(e) => handleInput(e, index)} 
-                            onKeyDown={(e) => handlekeyDown(e, index)} />
+                            ref={e => inputRefs.current[index] = e}        
+                            onInput={(e) => handleInput(e, index)}         //forward cursor
+                            onKeyDown={(e) => handlekeyDown(e, index)} />  //back space
                     ))}
                 </div>
                 <button className='w-full py-3 bg-gradient-to-r from-indigo-500 to-indigo-900 text-white rounded-full'>Verify email</button>
