97db60ceeabdd8ae2e5ae70819f5222118e29958
Your Name <gchkumar999@gmail.com>
2025-03-21 23:05:58 +0530

    txt

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+
 const connectDB=async ()=>{
     mongoose.connection.on('connected',()=>console.log("Data base connected"))
     await mongoose.connect(`${process.env.MONGODB_URI}/mern-auth`)
