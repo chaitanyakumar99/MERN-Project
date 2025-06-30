402bc67601e789839538d5cdd09c93798cdc8c80
Your Name <gchkumar999@gmail.com>
2025-06-30 17:10:38 +0530

    ok

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import cors from '--'
+import cors from 'cors'
 import 'dotenv/config';
 import cookieparser from "cookie-parser";
 import connectDB from './config/mongodb.js';
